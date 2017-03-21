dbb64ac9f96997fac8314eda61c8b1f0492cca21
AlexG <alexg3178@gmail.com>
2017-03-21 12:17:00 +0200

    Replace strings in Parser with constants.

--- app/src/main/java/com/example/test/colors/ColorParser.java
+++ app/src/main/java/com/example/test/colors/ColorParser.java
@@ -1,21 +1,24 @@
 package com.example.test.colors;
 
-
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class ColorParser {
-    public ArrayList<CustomColor> parseCustomColors(XmlPullParser parser) {
+class ColorParser {
+
+    private final String COLOR_NAME = "name";
+    private final String COLOR_VALUE = "color";
+
+    ArrayList<CustomColor> parseCustomColors(XmlPullParser parser) {
         ArrayList<CustomColor> colors = new ArrayList<>();
         try {
             while (parser.getEventType() != XmlPullParser.END_DOCUMENT) {
                 switch (parser.getEventType()) {
                     case XmlPullParser.START_TAG:
-                        String name = parser.getAttributeValue(null, "name");
-                        String value = parser.getAttributeValue(null, "color");
+                        String name = parser.getAttributeValue(null, COLOR_NAME);
+                        String value = parser.getAttributeValue(null, COLOR_VALUE);
                         if (name != null && !name.isEmpty() && value != null && !value.isEmpty()) {
                             String nameCapitalized = ColorUtils.capitalize(name);
                             colors.add(new CustomColor(nameCapitalized, value));
